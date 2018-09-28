2afd674ee3bd6b8c47a786050641fdcd581f2c45
ande4694 <knudsen879@hotmail.com>
2018-09-28 11:36:04 +0200

    print uden lambda

--- src/MyStringArray.java
+++ src/MyStringArray.java
@@ -117,6 +117,9 @@ public class MyStringArray implements MyList {
         // lamda expressions
         // flere metoder på et kald
 
+//        for (int i = 0; i < size; i++){
+//            System.out.println(get(i));
+//        }
 
     }
 
